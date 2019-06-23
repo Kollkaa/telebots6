f9bfe3422e16625afef593ed45a7a2e897a5c77a
kolun <1kolia1.com@gmail.com>
2019-06-23 19:26:52 +0300

    telebot

---
+++ src/main/java/Application.java
@@ -0,0 +1,105 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class Application {
+    static Bot bot;
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        System.out.println("contunie");
+        try//
+        {
+            bot=new Bot();
+            telegramBotsApi.registerBot(bot);
+
+
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+        SpringApplication.run(Application.class, args);
+        final ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        ses.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Test();
+
+                }catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                System.out.println("1111111111111111111111111111");
+            }
+        }, 0, 20, TimeUnit.MINUTES);
+
+
+    }
+    public static void Test()
+    {
+
+        String HTMLSTring = "<!DOCTYPE html>"
+                + "<html>"
+                + "<head>"
+                + "<title>JSoup Example</title>"
+                + "</head>"
+                + "<body>"
+                + "<table><tr><td><h1>HelloWorld</h1></tr>"
+                + "</table>"
+                + "</body>"
+                + "</html>";
+
+        Document html = Jsoup.parse(HTMLSTring);
+        String title = html.title();
+        String h1 = html.body().getElementsByTag("h1").text();
+
+        System.out.println("Input HTML String to JSoup :" + HTMLSTring);
+        System.out.println("After parsing, Title : " + title);
+        System.out.println("Afte parsing, Heading : " + h1);
+
+        // JSoup Example 2 - Reading HTML page from URL
+        Document doc;
+        try {
+            doc = Jsoup.connect("https://apps2321.herokuapp.com/app").get();
+            title = doc.title();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Jsoup Can read HTML page from URL, title : " + title);
+
+        // JSoup Example 3 - Parsing an HTML file in Java
+        //Document htmlFile = Jsoup.parse("login.html", "ISO-8859-1"); // wrong
+        Document htmlFile = null;
+        try {
+            htmlFile = Jsoup.parse(new File("login.html"), "ISO-8859-1");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } // right
+        title = htmlFile.title();
+        Element div = htmlFile.getElementById("login");
+        String cssClass = div.className(); // getting class form HTML element
+
+        System.out.println("Jsoup can also parse HTML file directly");
+        System.out.println("title : " + title);
+        System.out.println("class of div tag : " + cssClass);
+
+    }
+
+}
---
+++ src/main/java/MainController.java
@@ -0,0 +1,22 @@
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/app")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "app";
+    }
+    @GetMapping("/users")
+    public String users(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "users";
+    }
+
+}
