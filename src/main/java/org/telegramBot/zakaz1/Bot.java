83de2912196f55435105cf4a4739d9a525726c1d
kolun <1kolia1.com@gmail.com>
2019-06-24 12:09:35 +0300

    telebot

--- src/main/java/org/telegramBot/zakaz1/Bot.java
+++ src/main/java/org/telegramBot/zakaz1/Bot.java
@@ -1,7 +1,9 @@
 package org.telegramBot.zakaz1;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
     @Override
@@ -17,6 +19,11 @@ public class Bot extends TelegramLongPollingBot {
             {a=3;}
          switch (a){
              case 1:
+                 try {
+                     sendApiMethod(new SendMessage().setChatId(update.getMessage().getChatId()).setText("hi"));
+                 } catch (TelegramApiException e) {
+                     e.printStackTrace();
+                 }
                  break;
              case 2:
                  break;
