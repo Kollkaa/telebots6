c1b32296532eb8740a8475bb1bb57807bffec57d
BOSS <rudiak.denchik@gmail.com>
2019-07-20 12:17:45 +0300

    rudjia

--- src/main/java/org/telegramBot/zakaz1/Application.java
+++ src/main/java/org/telegramBot/zakaz1/Application.java
@@ -81,7 +81,7 @@ public class Application {
         // JSoup Example 2 - Reading HTML page from URL
         Document doc;
         try {
-            doc = Jsoup.connect("https://documentbot.herokuapp.com/app").get();
+            doc = Jsoup.connect("https://documentbot.herokuapp.com/documentbot").get();
             title = doc.title();
         } catch (IOException e) {
             e.printStackTrace();
